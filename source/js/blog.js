bb8d82861a7960ad1817a53f9d65aaae6e9e0c81
曾杰 <jasonzeng@anjuke.com>
2014-08-10 00:19:41 +0800

    theme update

--- source/js/blog.js
+++ source/js/blog.js
@@ -1,6 +1,31 @@
-console.log('here');
-
 (function () {
+		    // 回滚到顶部
+ 	var isShowed = false;
+   	$(window).scroll(function(e){
+   		var scr = document.documentElement.scrollTop || document.body.scrollTop;
+		if (scr > 400) {
+			// $('#back_top').show(400);
+			if (!isShowed) {
+
+				$('#back_top').css("display","block");
+				isShowed = true;
+				$('#back_top').on("click",function(){
+					$('body,html').animate({scrollTop:0},400);
+				});
+			};
+
+		}else{
+
+			if (isShowed) {
+				
+				$('#back_top').css("display","none");
+				$('#back_top').unbind("click");
+				isShowed = false;
+			};
+
+			// $('#back_top').hide(300);
+		}
+	});
 	$(document).pjax('a[data-pjax]', '.container', { fragment: '.container', timeout: 10000 });
 	$(function() {
     $("img.lazy").lazyload({
